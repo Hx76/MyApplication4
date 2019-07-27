0a31b3d7607d029151d14552bba58d1cc06ef8da
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-07-27 15:18:57 +0800

    广播

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -9,26 +10,46 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+    private LocalReceiver localReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        /*intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver,intentFilter);
+        registerReceiver(networkChangeReceiver,intentFilter);*/
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* Intent intent = new Intent("com.example.myapplication.MY_BROADCAST");
+                //sendBroadcast(intent);
+                sendOrderedBroadcast(intent,null);*/
+               Intent intent = new Intent("com.example.myapplication.LOCAL_BROADCAST");
+               localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+        intentFilter.addAction("com.example.myapplication.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        //unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
     class NetworkChangeReceiver extends BroadcastReceiver {
@@ -42,6 +63,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
             }
 
+        }
+    }
+
+    class LocalReceiver extends  BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"enenenenen",Toast.LENGTH_SHORT).show();
         }
     }
 }
